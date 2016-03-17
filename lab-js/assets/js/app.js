4b617257dd0f13041a6c6873a48897b6e01e986a
brinnathomsen <thomb212@gmail.com>
2016-03-17 09:07:18 -0400

    quiz 3

--- lab-js/assets/js/app.js
+++ lab-js/assets/js/app.js
@@ -1,1 +1,26 @@
-alert("hello js")
+// alert("hello js")
+
+
+var animal = "cat";
+var age = 12;
+var clothing = ["shirts", "socks", "pants"];
+var winner = true;
+var checkingAccount = 1000;
+var marniShirt = 1000;
+var stellaDress = 3000;
+
+
+
+console.log(winner);
+
+
+
+
+
+function charge_cc(item) {
+  alert('your account will be charged ' + item);
+  alert('your account balance is ' + checkingAccount);
+if (checkingAccount < item )
+alert('you do not have any money');
+
+}
