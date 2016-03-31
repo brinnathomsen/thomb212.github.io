ed3d66c2fed34452a8fe3df4411e5874b417e1d8
brinnathomsen <thomb212@gmail.com>
2016-03-31 10:56:43 -0400

    experimenting with pixelated curves for typeface, js exercise

--- project-2/typeface-site/assets/js/app.js
+++ project-2/typeface-site/assets/js/app.js
@@ -1,43 +1,122 @@
-var tilt;
-var color;
-var canvas;
+// Allowing the user to type in each letter
 
-function setup() {
-  canvas = createCanvas(windowWidth, windowHeight);
-  canvas.position(0, 0);
-  noLoop();
-  noStroke();
-  fill(255, 255, 255);
-  tilt = random(0, 10);
-  color = random(0, 255);
-}
-
-function draw() {
-
-  colorMode(HSB);
-  background(random(0, 300), random(100, 255), 100);
-
-
-  for (var i = 0; i < 150; i++) {
-    drawScribbles(random(10, 600), random(10, 600), random(1, 20), random(30,
-      80));
+$(window).keypress(function(event) {
+  var newLetter;
+  if (event.which == 97 || event.which == 65) {
+    newLetter = $(".letter-wrapper .a").clone();
+    $(".stage").append(newLetter);
   }
-}
 
-function drawScribbles(x, y, segmentLength, steps) {
-
-  for (var step = 0; step < steps; step++) {
-    stroke(random(color, color + 100), 5 * step, random(200, 255));
-    strokeWeight(random(1, 5));
-
-    var nextX, nextY;
-    nextX = x + random(-segmentLength / tilt, segmentLength * tilt);
-    nextY = y + random(-segmentLength / tilt, segmentLength * tilt);
-
-    noFill();
-    curve(random(500, 650), 200, x, y, nextX, nextY, 400, 300);
-
-    x = nextX;
-    y = nextY;
+  if (event.which == 98) {
+    newLetter = $(".letter-wrapper .b").clone();
+    $(".stage").append(newLetter);
   }
-}
+
+  if (event.which == 99) {
+    newLetter = $(".letter-wrapper .c").clone();
+    $(".stage").append(newLetter);
+  }
+
+  if (event.which == 100) {
+    newLetter = $(".letter-wrapper .d").clone();
+    $(".stage").append(newLetter);
+  }
+
+  if (event.which == 101) {
+    newLetter = $(".letter-wrapper .e").clone();
+    $(".stage").append(newLetter);
+  }
+
+  if (event.which == 102) {
+    newLetter = $(".letter-wrapper .f").clone();
+    $(".stage").append(newLetter);
+  }
+
+  if (event.which == 103) {
+    newLetter = $(".letter-wrapper .g").clone();
+    $(".stage").append(newLetter);
+  }
+  if (event.which == 104) {
+    newLetter = $(".letter-wrapper .h").clone();
+    $(".stage").append(newLetter);
+  }
+  if (event.which == 105) {
+    newLetter = $(".letter-wrapper .i").clone();
+    $(".stage").append(newLetter);
+  }
+  if (event.which == 106) {
+    newLetter = $(".letter-wrapper .j").clone();
+    $(".stage").append(newLetter);
+  }
+  if (event.which == 107) {
+    newLetter = $(".letter-wrapper .k").clone();
+    $(".stage").append(newLetter);
+  }
+  if (event.which == 108) {
+    newLetter = $(".letter-wrapper .l").clone();
+    $(".stage").append(newLetter);
+  }
+  if (event.which == 109) {
+    newLetter = $(".letter-wrapper .m").clone();
+    $(".stage").append(newLetter);
+  }
+  if (event.which == 110) {
+    newLetter = $(".letter-wrapper .n").clone();
+    $(".stage").append(newLetter);
+  }
+  if (event.which == 111) {
+    newLetter = $(".letter-wrapper .o").clone();
+    $(".stage").append(newLetter);
+  }
+  if (event.which == 112) {
+    newLetter = $(".letter-wrapper .p").clone();
+    $(".stage").append(newLetter);
+  }
+  if (event.which == 113) {
+    newLetter = $(".letter-wrapper .q").clone();
+    $(".stage").append(newLetter);
+  }
+  if (event.which == 114) {
+    newLetter = $(".letter-wrapper .r").clone();
+    $(".stage").append(newLetter);
+  }
+  if (event.which == 115) {
+    newLetter = $(".letter-wrapper .s").clone();
+    $(".stage").append(newLetter);
+  }
+  if (event.which == 116) {
+    newLetter = $(".letter-wrapper .t").clone();
+    $(".stage").append(newLetter);
+  }
+  if (event.which == 117) {
+    newLetter = $(".letter-wrapper .u").clone();
+    $(".stage").append(newLetter);
+  }
+  if (event.which == 118) {
+    newLetter = $(".letter-wrapper .v").clone();
+    $(".stage").append(newLetter);
+  }
+  if (event.which == 119) {
+    newLetter = $(".letter-wrapper .w").clone();
+    $(".stage").append(newLetter);
+  }
+  if (event.which == 120) {
+    newLetter = $(".letter-wrapper .x").clone();
+    $(".stage").append(newLetter);
+  }
+  if (event.which == 121) {
+    newLetter = $(".letter-wrapper .y").clone();
+    $(".stage").append(newLetter);
+  }
+  if (event.which == 122) {
+    newLetter = $(".letter-wrapper .z").clone();
+    $(".stage").append(newLetter);
+  }
+
+  if (event.which == 32) {
+    newLetter = $(".letter-wrapper .letter .space").clone();
+    $("stage").append(newLetter);
+  }
+
+  console.log(event.which);
+});
