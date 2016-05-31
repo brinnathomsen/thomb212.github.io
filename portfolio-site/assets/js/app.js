2c64997e2cacbb0d84b08ec0df7fb732c77e1673
brinnathomsen <thomb212@gmail.com>
2016-05-30 22:23:29 -0400

    updates

---
+++ portfolio-site/assets/js/app.js
@@ -0,0 +1,51 @@
+// $(".spiral").mouseenter(
+//   $(".spiral").css("opacity", 0)
+// ).mouseleave(
+//   $(".spiral").css("opacity", 100)
+// );
+
+
+
+$(".spiral").hover(function() {
+  $(".spiral").css("opacity", ".75");
+
+}, function() {
+  $(".spiral").css("opacity", "1");
+});
+
+$(".identity").hover(function() {
+  $(".identity").css("opacity", ".75");
+}, function() {
+  $(".identity").css("opacity", "1");
+});
+
+
+$(".scribbles").hover(function() {
+  $(".scribbles").css("opacity", ".75");
+}, function() {
+  $(".scribbles").css("opacity", "1");
+});
+
+$(".identity").hover(function() {
+  $(".identity").css("opacity", ".75");
+}, function() {
+  $(".identity").css("opacity", "1");
+});
+
+$(".pixels").hover(function() {
+  $(".pixels").css("opacity", ".75");
+}, function() {
+  $(".pixels").css("opacity", "1");
+});
+
+$(".cube").hover(function() {
+  $(".cube").css("opacity", ".75");
+}, function() {
+  $(".cube").css("opacity", "1");
+});
+
+$(".emotional").hover(function() {
+  $(".emotional").css("opacity", ".75");
+}, function() {
+  $(".emotional").css("opacity", "1");
+});
